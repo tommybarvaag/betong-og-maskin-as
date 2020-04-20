5384d1a3c720575a0ae54ea441d1430d1bc2a773
Tommy Barvåg <tommy@barvaag.com>
2020-04-20 09:14:14 +0200

    Only load google map when in viewport

--- web/src/components/googleMap.js
+++ web/src/components/googleMap.js
@@ -1,35 +1,48 @@
 import { GoogleMap as ReactGoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import React from "react";
+import handleViewport from "react-in-viewport";
 
-export default function GoogleMap(props) {
-  const { latitude, longitude } = props;
+function GoogleMapBase(props) {
+  const { latitude, longitude, inViewport, forwardedRef } = props;
+  const [load, setLoad] = React.useState(false);
+
+  React.useEffect(() => {
+    if (inViewport) {
+      setLoad(true);
+    }
+  }, [inViewport]);
 
   const googleMapsApiKey = process.env.BOM_GOOGLE_MAPS_API_KEY;
 
   return (
-    googleMapsApiKey !== null &&
-    googleMapsApiKey !== undefined && (
-      <LoadScript id="script-loader" googleMapsApiKey={googleMapsApiKey}>
-        <ReactGoogleMap
-          id="bom-map"
-          mapContainerStyle={{
-            height: "600px",
-            width: "100%"
-          }}
-          zoom={14}
-          center={{
-            lat: latitude,
-            lng: longitude
-          }}
-        >
-          <Marker
-            position={{
+    <div ref={forwardedRef}>
+      {googleMapsApiKey !== null && googleMapsApiKey !== undefined && load && (
+        <LoadScript id="script-loader" googleMapsApiKey={googleMapsApiKey}>
+          <ReactGoogleMap
+            id="bom-map"
+            mapContainerStyle={{
+              height: "600px",
+              width: "100%"
+            }}
+            zoom={14}
+            center={{
               lat: latitude,
               lng: longitude
             }}
-          />
-        </ReactGoogleMap>
-      </LoadScript>
-    )
+          >
+            <Marker
+              position={{
+                lat: latitude,
+                lng: longitude
+              }}
+            />
+          </ReactGoogleMap>
+        </LoadScript>
+      )}
+    </div>
   );
 }
+
+const GoogleMap = handleViewport(GoogleMapBase, { rootMargin: "-1.0px" });
+
+export default GoogleMap;
